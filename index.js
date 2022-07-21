19d3953d7e31538819dd248bade02acaa9267a82
Philipp Jenny <register@philippjenny.de>
2022-07-21 11:42:48 +0200

    revert

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/chefkoch/:recipieURL", (req, res) => {
       var schritte = "";
 
       $(".ds-input", html).each(function () {
-        portions = $(this).find.attr("value");
+        portions = $(this).attr("value");
         console.log(portions);
       });
 
